32aeb431aba44316df51f1bfdb973c2990ef6bd5
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-12 14:23:38 +0900

    rollupの設定を変更

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,18 @@
 import typescript from '@rollup/plugin-typescript';
+import pkg from "./package.json";
+
+const moduleName = upperFirst(camelCase(pkg.name.replace(/^\@.*\//, '')));
 
 export default {
   input: 'lib/run.ts',
   output: [
     {
+      name: moduleName,
       file: 'dist/bundle.js',
       format: 'cjs',
     },
   ],
   plugins: [
     typescript(),
-    // pluginCommonjs({
-    //   extensions: ['.js', '.ts'],
-    // }),
-    // pluginBabel({
-    //   babelHelpers: 'bundled',
-    //   configFile: path.resolve(__dirname, '.babelrc.js'),
-    // }),
-    // pluginNodeResolve({
-    //   browser: true,
-    // }),
   ],
 };
