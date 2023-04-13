7d777d363c58c1828d907f8a0dff112710dd476f
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-13 19:26:18 +0900

    一旦保存

--- lib/run.ts
+++ lib/run.ts
@@ -2,20 +2,20 @@ import { exploration } from './exploration/main';
 
 const data = { hoge: 'hoge', fuga: { hoge1: 'hoge1', hoge2: 'hoge2', array: ['array', 'array2'] } };
 
-// const data = [1, 2, 3, 4, [1, 2, 3, 4, 5], [6, 7, 8, 9]];
 exploration(data, {
-  callback: (value, handle) => {
-    console.log('結果', value);
-    if (value.value === 'hoge') {
+  callback: (item, handle) => {
+    console.log('結果', item);
+    if (item.value === 'hoge') {
       handle.stop();
     }
   },
 });
 
-exploration(data, {
-  callback: (value, handle) => {
-    console.log('結果', value);
-    if (value.value === 'array') {
+const data2 = [1, 2, 3, 4, [1, 2, 88, 4, 5], [6, 7, 8, 9]];
+exploration(data2, {
+  callback: (item, handle) => {
+    console.log('結果', item);
+    if (item.value === 88) {
       handle.stop();
     }
   },
