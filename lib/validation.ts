66151b9c379eb5ba16788d2e578eca064f1b230a
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-16 15:20:51 +0900

    Validation関数をリファクタリング

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,21 +1,6 @@
 import { IValidatePlugin } from './types';
 import { convertPluginToValidates } from './validate';
 
-export class Validation {
-  plugins: IValidatePlugin[] = [];
-  constructor(plugins: IValidatePlugin[]) {
-    this.plugins = plugins;
-  }
-
-  run(target: unknown) {
-    const validates = convertPluginToValidates(this.plugins);
-    for (const validate of validates) {
-      if (validate.check(target)) return true;
-    }
-    return false;
-  }
-}
-
 /**
  * validation.
  *
@@ -24,5 +9,8 @@ export class Validation {
  * @returns {boolean} Return true if there is an error in validation, and false if there is no error.
  */
 export function validation(validatePlugins: IValidatePlugin[], target: unknown): boolean {
-  return new Validation(validatePlugins).run(target);
+  for (const validate of convertPluginToValidates(validatePlugins)) {
+    if (validate.check(target)) return true;
+  }
+  return false;
 }
