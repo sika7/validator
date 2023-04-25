ed9ecd019bdcc77d753481169ff15b372e4f9723
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-25 15:28:03 +0900

    プラグインのbuild方法を修正

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,46 @@
+import glob from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import typescript from '@rollup/plugin-typescript';
 
-export default {
-  input: 'lib/run.ts',
-  output: [
-    {
-      file: 'dist/bundle.cjs',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/bundle.mjs',
-      format: 'es',
-    },
-  ],
-  plugins: [
-    typescript(),
-  ],
-};
+const pluginFiles = Object.fromEntries(
+  glob
+    .sync('lib/plugins/**/*.ts')
+    .map((file) => [
+      path.relative('lib/plugins', file.slice(0, file.length - path.extname(file).length)),
+      fileURLToPath(new URL(file, import.meta.url)),
+    ])
+);
+
+export default [
+  {
+    input: 'lib/export.ts',
+    output: [
+      {
+        file: 'dist/export.cjs',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/export.mjs',
+        format: 'es',
+      },
+    ],
+    plugins: [typescript()],
+  },
+  {
+    input: pluginFiles,
+    output: [
+      {
+        dir: 'dist/plugin/',
+        entryFileNames: '[name].cjs',
+        format: 'cjs',
+      },
+      {
+        dir: 'dist/plugin/',
+        entryFileNames: '[name].mjs',
+        format: 'es',
+      },
+    ],
+    plugins: [typescript()],
+  },
+];
