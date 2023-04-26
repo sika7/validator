c4da656425b31910ce5e50adc1ed325e7c134c26
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-26 23:43:36 +0900

    整数値判定を追加

---
+++ lib/plugins/isInteger.ts
@@ -0,0 +1,12 @@
+import { ValidatePlugin } from '../types';
+
+export function isInteger(): ValidatePlugin {
+  return {
+    name: 'integer',
+    errorMessage: 'not integer',
+    validation: (value) => {
+      if (typeof value !== 'number') return false;
+      return Number.isInteger(value);
+    },
+  };
+}
