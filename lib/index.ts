803da2f1ffae56b66b872e39385c1fbd7416f355
sika7 <91676209+sika7@users.noreply.github.com>
2023-04-30 22:45:26 +0900

    pluginの型も追加

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,8 @@ export { DictionaryValidator } from './dictionaryValidator';
 export { Validate } from './validate';
 export { validation } from './validation';
 export { validator, Validator } from './validator';
+export { isString } from './plugins/isString';
+export { isNumber } from './plugins/isNumber';
+export { isInteger } from './plugins/isInteger';
+export { isHttps } from './plugins/isHttps';
+export { isUrl } from './plugins/isUrl';
